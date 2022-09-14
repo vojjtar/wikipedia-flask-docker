80b5abf32e541d2e6a0a1dac15f51c8a953e0888
vojta <sopajyt@gmail.com>
2022-09-14 22:18:26 +0200

    added: more functionality

--- wikipedia_app/routes.py
+++ wikipedia_app/routes.py
@@ -1,7 +1,41 @@
 from wikipedia_app import app
 
-from flask import render_template
+from flask import render_template, request
+from wikipedia_app import wiki
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+from wikipedia_app.wiki import get_random_wikipedia_article
+from datetime import date
+
+@app.route('/', methods=["GET", "POST"])
+def index() -> render_template:
+    wiki_result = None
+    success = None
+    show_text_div = False
+
+    if  request.method == "POST":
+        search_query = request.form['query']
+        sentences = request.form['sentences']
+        language = request.form['language']
+
+        wiki_result = get_random_wikipedia_article(search_query, language, int(sentences))
+
+        print(wiki_result)
+
+        if (wiki_result[-1] == True):
+            success = True
+        elif (wiki_result[-1] == False):
+            success = False
+        elif (wiki_result[-1] == 'not_found'):
+            success = 'not_found'
+    
+
+        show_text_div = True
+        wiki_result = wiki_result[0]
+
+
+
+    return render_template('index.html',
+                            date=date.today().year,
+                            wiki_result=wiki_result,
+                            success=success,
+                            show_text_div=show_text_div)
--- wikipedia_app/wiki.py
+++ wikipedia_app/wiki.py
@@ -1,6 +1,11 @@
 import wikipedia
 
-wikipedia.set_lang('cz')
+def get_random_wikipedia_article(search_query: str, language: str, sentences: int) -> str:
+    wikipedia.set_lang(language)
+    try:
+        return wikipedia.summary(search_query, sentences=sentences, auto_suggest=True), True
+    except wikipedia.exceptions.DisambiguationError as e:
+        return e.args, False
+    except wikipedia.exceptions.PageError as e:
+        return e.args, 'not_found'
 
-def get_random_wikipedia_article() -> str:
-    return wikipedia.summary('https://en.wikipedia.org/wiki/Special:Random', 4)
